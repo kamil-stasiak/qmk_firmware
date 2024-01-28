a7ced6c542c4c721fdf01115d83a2882316d0d94
Kamil Stasiak <kamilstasiaknew@gmail.com>
2024-01-28 15:48:57 +0100

    basic layout

--- keyboards/prototype3/keymaps/default/keymap.c
+++ keyboards/prototype3/keymaps/default/keymap.c
@@ -6,26 +6,26 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐               ┌───┬───┬───┬───┬───┬───┐
-      * │ P │ 1 │ 2 │ 3 │ 4 │ 5 │               │ 6 │ 7 │ 8 │ 9 │ 0 │ Q │
+      * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │               │ 6 │ 7 │ 8 │ 9 │ 0 │ Q │
       * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
-      * │ O │ Q │ W │ E │ R │ T │               │ Y │ U │ I │ O │ P │ \ │
+      * │TAB│ Q │ W │ E │ R │ T │               │ Y │ U │ I │ O │ P │ \ │
       * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
-      * │ I │ A │ S │ D │ F │ G │               │ H │ J │ K │ L │ ; │ ' │
+      * │BCS│ A │ S │ D │ F │ G │               │ H │ J │ K │ L │ ; │ ' │
       * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
-      * │ U │ Z │ X │ C │ V │ B │               │ N │ M │ , │ . │ / │ ] │
+      * │???│ Z │ X │ C │ V │ B │               │ N │ M │ , │ . │ / │ ] │
       * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
-      * │ Y │ H │ J │ K │ L │ ; │               │ A │ B │ V │ C │ X │ \ │
+      * │???│???│???│UP │SPC│ENT│               │???│SPC│UP │???│???│???│
       * ├───┼───┼───┼───┼───┼───┤               ├───┼───┼───┼───┼───┼───┤
-      * │ V │ B │ N │ M │ , │ ' │               │ A │ B │ V │ C │ X │ \ │
+      * │   │   │   │   │   │ ' │               │???│   │   │   │   │   │
       * └───┴───┴───┴───┴───┴───┘               └───┴───┴───┴───┴───┴───┘
       */
     [0] = LAYOUT_split_3x6_3(
-          KC_P,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,     KC_8,    KC_9,    KC_0,      KC_MINUS,
-          KC_O,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,      KC_BACKSLASH,
-          KC_I,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,    KC_J,     KC_K,    KC_L, KC_SEMICOLON, KC_QUOTE,
-          KC_U,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,  KC_COMMA,   KC_DOT,  KC_SLASH,    KC_RIGHT_BRACKET,
-          KC_Y,    KC_H,    KC_J,    KC_K,    KC_L,  KC_SEMICOLON,          KC_B,    KC_V,     KC_C,    KC_X,    KC_Z,      KC_LEFT_BRACKET,
-          KC_V,    KC_B,    KC_N,    KC_M,    KC_COMMA,  KC_QUOTE,          KC_B,    KC_V,     KC_C,    KC_X,    KC_Z,      KC_BACKSLASH
+          KC_ESC,       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,     KC_8,    KC_9,    KC_0,      KC_MINUS,
+          KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,      KC_BACKSLASH,
+          KC_BACKSPACE, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,    KC_J,     KC_K,    KC_L, KC_SEMICOLON, KC_QUOTE,
+          KC_NO,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,  KC_COMMA,   KC_DOT,  KC_SLASH,  KC_RIGHT_BRACKET,
+          KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_SPACE,  KC_ENTER,          KC_NO,   KC_SPACE, KC_NO,   KC_NO,   KC_NO,     KC_NO,
+          KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_QUOTE,             KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO
     )
 };
 
